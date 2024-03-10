74b0466809b96b6ad0d38911ba84638361d56cce
pranavshirali <pranavshirali007@gmail.com>
2024-03-11 01:22:25 +0530

    added ass11f PrbNo.2

--- ass11.java
+++ ass11.java
@@ -2,6 +2,8 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ass11 {
 
@@ -19,6 +21,23 @@ public class ass11 {
         
         List<Integer> distinctUnion = new ArrayList<>(distinctElement);
         return distinctUnion;
+    }
+
+    //Problem No.2
+    public static int validIP(String ipAddress){
+        String zeroTo255
+            = "(25[0-5]|2[0-4]\\d|1\\d{2}|[1-9]?\\d)";
+                //25[0-5] --> 250-255
+                //2[0-4]\\d --> 200-249
+                //1\\d{2} --> 100-199
+                //[1-9]?\\d --> 0-99
+        String regex = zeroTo255 + "\\." + zeroTo255 + "\\." + zeroTo255 + "\\." + zeroTo255;
+    
+        Pattern pattern = Pattern.compile(regex);
+        Matcher match = pattern.matcher(ipAddress);
+        
+        if(match.matches()){return 1;}
+        else return 0;
     }
     
 }
